531d40d0332161b39e0fb857f54b713fb4ef4b3d
Irina-Gorges <r3nategr4nate@gmail.com>
2025-05-03 13:46:57 +0200

    refreshed css and js code

--- scripts/script.js
+++ scripts/script.js
@@ -5,36 +5,78 @@ function init() {
 async function fetchDataJson(pokeUrl) {
     const response = await fetch(pokeUrl);
     const responseAsJson = await response.json();
-    renderHtml(responseAsJson);
+    await renderHtml(responseAsJson);
 }
 
-function renderHtml(poke) {
-    saveToLocalStorage(poke.results);
-    let contentRef = document.getElementById("content");
+function saveToLocalStorage(pokeList) {
+    localStorage.setItem("pokemon", JSON.stringify(pokeList));
+}
+
+async function renderHtml(poke) {
+    const contentRef = document.getElementById("content");
     contentRef.innerHTML = "";
-    const pokeMon = getFromLocalStorage();
-    for (i = 0; i < pokeMon.length; i++) {
-        contentRef.innerHTML += getRenderHtmlTemplate(pokeMon[i].id, pokeMon[i].name);
-    }
-}
 
-function saveToLocalStorage(poke) {
-    let id = "";
-    for (i = 0; i < poke.length; i++) {
-        id = poke[i].url.match(/\/pokemon\/(.*)\//);
-        poke[i] = {
-            id: `${id[1]}`,
-            name: `${poke[i].name}`,
-            url: `${poke[i].url}`,
-        };
-        localStorage.setItem(id[1], JSON.stringify(poke[i]));
+    const allPokemon = [];
+
+    for (let i = 0; i < poke.results.length; i++) {
+        const res = await fetch(poke.results[i].url);
+        const data = await res.json();
+
+        allPokemon.push({
+            id: data.id,
+            name: data.name,
+            type: data.types[0].type.name, // nur der erste Typ
+            url: data.forms[0].url,
+        });
+    }
+
+    saveToLocalStorage(allPokemon);
+
+    for (let i = 0; i < allPokemon.length; i++) {
+        contentRef.innerHTML += getRenderHtmlTemplate(
+            allPokemon[i].id,
+            allPokemon[i].name,
+            allPokemon[i].type
+        );
     }
 }
 
 function getFromLocalStorage() {
-    let myArr = [];
-    for (i = 0; i < localStorage.length; i++) {
-        myArr[i] = JSON.parse(localStorage.getItem(i + 1));
-    }
-    return myArr;
+    return JSON.parse(localStorage.getItem("pokemon")) || [];
 }
+
+// function renderHtml(poke) {
+//     saveToLocalStorage(poke.results);
+//     let contentRef = document.getElementById("content");
+//     contentRef.innerHTML = "";
+//     const pokeMon = getFromLocalStorage();
+//     for (i = 0; i < pokeMon.length; i++) {
+//         contentRef.innerHTML += getRenderHtmlTemplate(
+//             pokeMon[i].id,
+//             pokeMon[i].name,
+//             pokeMon[i].types
+//         );
+//     }
+// }
+
+// function saveToLocalStorage(poke) {
+//     let id = "";
+//     for (i = 0; i < poke.length; i++) {
+//         id = poke[i].url.match(/\/pokemon\/(.*)\//);
+//         poke[i] = {
+//             id: `${id[1]}`,
+//             name: `${poke[i].name}`,
+//             url: `${poke[i].url}`,
+//             types: `${poke[i].types.type.name}`,
+//         };
+//         localStorage.setItem(id[1], JSON.stringify(poke[i]));
+//     }
+// }
+
+// function getFromLocalStorage() {
+//     let myArr = [];
+//     for (i = 0; i < localStorage.length; i++) {
+//         myArr[i] = JSON.parse(localStorage.getItem(i + 1));
+//     }
+//     return myArr;
+// }
--- scripts/template.js
+++ scripts/template.js
@@ -1,13 +1,8 @@
-function getRenderHtmlTemplate(id, name) {
+function getRenderHtmlTemplate(id, name, type) {
     return (
-        `<div id="${id}" class="poke_Container"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/shiny/` +
-        id +
-        `.gif">` +
-        `<div class="name_Info">` +
-        `#` +
-        id +
-        `  ` +
-        name +
-        `</div></div>`
+        `<div id="${id}" class="poke_Container">` +
+        `<img class="${type}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png">` +
+        `<div class="name_Info">#${id} &nbsp; ${name}</div>` +
+        `</div>`
     );
 }
