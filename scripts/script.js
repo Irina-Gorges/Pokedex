10c4e37fa2280a44c67f8daddcd0b1546e7f4344
Irina-Gorges <r3nategr4nate@gmail.com>
2025-05-03 18:22:02 +0200

    added footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,55 @@
 function init() {
-    fetchDataJson("https://pokeapi.co/api/v2/pokemon?limit=40&offset=0");
+    //fetchDataJson("https://pokeapi.co/api/v2/pokemon?limit=40&offset=0");
+    renderHtml();
 }
 
 async function fetchDataJson(pokeUrl) {
     const response = await fetch(pokeUrl);
     const responseAsJson = await response.json();
-    await renderHtml(responseAsJson);
+    return responseAsJson;
 }
 
 function saveToLocalStorage(pokeList) {
     localStorage.setItem("pokemon", JSON.stringify(pokeList));
 }
 
-async function renderHtml(poke) {
+async function renderHtml() {
     const contentRef = document.getElementById("content");
     contentRef.innerHTML = "";
+    let allPokemon = [];
+    if (localStorage.getItem("pokemon").length == null) {
+        const poke = await fetchDataJson(
+            "https://pokeapi.co/api/v2/pokemon?limit=40&offset=0"
+        );
 
-    const allPokemon = [];
+        for (let i = 0; i < poke.results.length; i++) {
+            const res = await fetch(poke.results[i].url);
+            const data = await res.json();
 
-    for (let i = 0; i < poke.results.length; i++) {
-        const res = await fetch(poke.results[i].url);
-        const data = await res.json();
+            allPokemon.push({
+                id: data.id,
+                name: data.name,
+                type: data.types,
+                url: data.forms[0].url,
+                stats: data.stats,
+                height: data.height,
+                weight: data.weight,
+                base_experience: data.base_experience,
+                abilities: data.abilities,
+            });
+        }
 
-        allPokemon.push({
-            id: data.id,
-            name: data.name,
-            type: data.types[0].type.name, // nur der erste Typ
-            url: data.forms[0].url,
-        });
+        saveToLocalStorage(allPokemon);
+    } else {
+        allPokemon = getFromLocalStorage();
     }
-
-    saveToLocalStorage(allPokemon);
 
     for (let i = 0; i < allPokemon.length; i++) {
         contentRef.innerHTML += getRenderHtmlTemplate(
             allPokemon[i].id,
             allPokemon[i].name,
             allPokemon[i].type
+            // readArray(allPokemon[i].stats)
         );
     }
 }
@@ -45,38 +58,12 @@ function getFromLocalStorage() {
     return JSON.parse(localStorage.getItem("pokemon")) || [];
 }
 
-// function renderHtml(poke) {
-//     saveToLocalStorage(poke.results);
-//     let contentRef = document.getElementById("content");
-//     contentRef.innerHTML = "";
-//     const pokeMon = getFromLocalStorage();
-//     for (i = 0; i < pokeMon.length; i++) {
-//         contentRef.innerHTML += getRenderHtmlTemplate(
-//             pokeMon[i].id,
-//             pokeMon[i].name,
-//             pokeMon[i].types
-//         );
-//     }
-// }
+function readArray(myArr) {
+    let result = "";
 
-// function saveToLocalStorage(poke) {
-//     let id = "";
-//     for (i = 0; i < poke.length; i++) {
-//         id = poke[i].url.match(/\/pokemon\/(.*)\//);
-//         poke[i] = {
-//             id: `${id[1]}`,
-//             name: `${poke[i].name}`,
-//             url: `${poke[i].url}`,
-//             types: `${poke[i].types.type.name}`,
-//         };
-//         localStorage.setItem(id[1], JSON.stringify(poke[i]));
-//     }
-// }
+    for (i = 0; i < myArr.length; i++) {
+        result += myArr[i].stat.name + ": " + myArr[i].base_stat + "<br>";
+    }
+    return result;
+}
 
-// function getFromLocalStorage() {
-//     let myArr = [];
-//     for (i = 0; i < localStorage.length; i++) {
-//         myArr[i] = JSON.parse(localStorage.getItem(i + 1));
-//     }
-//     return myArr;
-// }
--- scripts/template.js
+++ scripts/template.js
@@ -1,8 +1,8 @@
-function getRenderHtmlTemplate(id, name, type) {
+function getRenderHtmlTemplate(id, name, type, stats) {
     return (
         `<div id="${id}" class="poke_Container">` +
-        `<img class="${type}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png">` +
-        `<div class="name_Info">#${id} &nbsp; ${name}</div>` +
+        `<img class="${type[0].type.name} photo" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png">` +
+        `<div class="name_Info">#${id} &nbsp; ${name}&nbsp; ${stats}</div>` +
         `</div>`
     );
 }
